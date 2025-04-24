f5134e556e9039edb2571d52a5dc51ee5e27ee7f
Sreeram <sreeramk282k@gmail.com>
2025-04-24 17:42:44 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Install Python packages
+pip install -r requirements.txt
+
+# Create local nltk_data dir and download punkt
 mkdir -p nltk_data
 python -m nltk.downloader -d nltk_data punkt
